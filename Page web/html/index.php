568467884a87fafe7d5398010c1476719767d610
Rhudaur <katsumotoshanguin@gmail.com>
2018-11-23 09:49:38 +0100

    r20

--- Page web/html/index.php
+++ Page web/html/index.php
@@ -95,7 +95,7 @@
             document.getElementById(String(tableResponses[i])).disabled = false;//!this.checked;
         }
     }
-	var r_primaryQuestion = [4.2,12.1,13.1,39.1,62.2];
+	var r_primaryQuestion = [4.2,12.1,13.1,39.2,39.3,62.2];
 	var r_questionTable = [ [5.1,6.1,6.2,6.3,7.1,7.2,7.3,8.1,8.2,8.3,9.1,9.2,9.3,10.1,10.2,10.3,11.1,11.2,11.3,11.4],
 							[13.1,13.2,14.1,14.2,14.3,15.1,15.2,15.3,16.1,16.2,16.3,16.4,17.1,17.2,17.3,17.4,18.1,18.2,18.3,19.1,19.2,19.3,20.1,20.2,20.3,21.1,21.2,21.3,22.1,22.2,22.3,23.1,
 							 23.2,23.3,24.1,24.2,24.3,25.1,25.2,25.3,26.1,26.2,26.3,27.1,27.2,27.3,28.1,28.2,28.3,29.1,29.2,29.3,30.1,30.2,30.3,31.1,31.2,31.3,32.1,32.2,32.3,33.1,33.2,33.3,
@@ -103,19 +103,20 @@
 							[14.1,14.2,14.3,15.1,15.2,15.3,16.1,16.2,16.3,16.4,17.1,17.2,17.3,17.4,18.1,18.2,18.3,19.1,19.2,19.3,20.1,20.2,20.3,21.1,21.2,21.3,22.1,22.2,22.3,23.1,
 							 23.2,23.3,24.1,24.2,24.3,25.1,25.2,25.3,26.1,26.2,26.3,27.1,27.2,27.3,28.1,28.2,28.3,29.1,29.2,29.3,30.1,30.2,30.3],
 							[40.1,41.1,42.1,42.2,42.3,42.4,43.1,43.2,43.3,43.4,44.1,44.2,44.3,45.1,45.2,45.3,46.1,46.2,46.3,47.1,47.2,47.3,47.4,48.1,48.2,48.3,49.1,49.2,49.3],
+							[40.1,41.1,42.1,42.2,42.3,42.4,43.1,43.2,43.3,43.4,44.1,44.2,44.3,45.1,45.2,45.3,46.1,46.2,46.3,47.1,47.2,47.3,47.4,48.1,48.2,48.3,49.1,49.2,49.3],
 							[64.1,64.2,64.3,65.1,65.2,65.3,66.1,66.2,66.3,67.1,67.2,67.3,68.1,68.2,68.3,69.1,69.2,69.3]];
 	r_listenerTable=[]
 	for(var i = 0; i < r_primaryQuestion.length; i++){
-        r_listenerTable.push(document.getElementById(r_primaryQuestion[i]));//ajoute elem fin tableau et retourne nvelle taille listenerTable
+        r_listenerTable.push(document.getElementById(r_primaryQuestion[i]));
         r_listenerTable[i].addEventListener("change",function(){
             i=0;
-            while(i<primaryQuestion.length){//while sert à vérifier la validité de i
-                if(r_primaryQuestion[i]== this.id){ //role this.id id de eventListener
-                    break;//sortie du while
+            while(i<primaryQuestion.length){
+                if(r_primaryQuestion[i]== this.id){ 
+                    break;
                 }
                 i++;
             }
-            //r_enabler(r_primaryQuestion[i],questionTable[i]); //cause problèmes : appel for (enabler) puis directement for (r_enabler)
+            r_enabler(r_primaryQuestion[i],r_questionTable[i]); //cause problèmes : appel for (enabler) puis directement for (r_enabler)
         });
     }
 	
